fa4649a7f4a5387c0a1059bb85a81c05148f295b
Michał <m.majewski.dm@gmail.com>
2024-06-05 16:47:12 +0200

    finished tasks

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,11 @@
-const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+const randomNumber = Math.round(Math.random() * 20)
+console.log(randomNumber)
+
+if (randomNumber > 5) {
+	console.log('Wypisuję liczby od 5 do ' + randomNumber + ':')
+	for (let number = 5; number <= randomNumber; number++) {
+		console.log(number)
+	}
+} else {
+	console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli.')
+}
--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,17 @@
-const x = 5;
-let result = 0;
+const x = 5
+let result = 0
+
+// pętla FOR
+for (let i = 1; i <= x; i++) {
+	result += i
+}
+
+//petla WHILE
+// let i = 1;
+
+// while (i <= x) {
+//     result += i;
+//     i++;
+// }
+
+console.log('Suma liczb od 1 do ' + x + ' wynosi: ' + result)
